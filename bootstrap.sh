43bb7279a569a7158c84b31edd8f66a8e61aa6c9
Lucas Vienna <dev@lucasvienna.dev>
2019-03-30 10:33:11 +0100

    added missing git stow and terminfo setup

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# This has been shamelessly copied from mathiasbynens/dotfiles
+# This has been shamelessly copied from mathiasbynens/dotfiles and edited a bit
 # https://github.com/mathiasbynens/dotfiles/blob/b22c32290e1518c0f228afba254ee6a3f6ab6d7a/bootstrap.sh
 
 cd $(dirname "${BASH_SOURCE}");
@@ -23,7 +23,13 @@ function doIt() {
   stow tmux;
   stow nvim;
   stow zsh;
+  stow git;
   spaceship;
+}
+
+function italicTerm() {
+  tic -x xterm-256color-italic.terminfo;
+  tic -x tmux-256color.terminfo;
 }
 
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
@@ -33,6 +39,7 @@ else
   echo "";
   if [[ $REPLY =~ ^[Yy]$ ]]; then
     doIt;
+    italicTerm;
   fi;
 fi;
 
