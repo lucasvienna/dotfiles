21ce5780f5d58c6f691eab1e1ce12a0e843aea8b
Lucas Vienna <vienna@edomo.de>
2019-08-05 15:56:35 +0200

    fixed terminfo sourcing

--- bootstrap.sh
+++ bootstrap.sh
@@ -28,8 +28,8 @@ function doIt() {
 }
 
 function italicTerm() {
-  tic -x xterm-256color-italic.terminfo;
-  tic -x tmux-256color.terminfo;
+  tic -x terminfo/xterm-256color-italic.terminfo;
+  tic -x terminfo/tmux-256color.terminfo;
 }
 
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
