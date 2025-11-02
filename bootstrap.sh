a3f75583c2960cd3a84e3daf662dee6d7f0b33e8
Lucas Vienna <dev@lucasvienna.dev>
2025-11-02 12:37:47 +0100

    revamp with v2

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,15 +3,14 @@
 # This has been shamelessly copied from mathiasbynens/dotfiles and edited a bit
 # https://github.com/mathiasbynens/dotfiles/blob/b22c32290e1518c0f228afba254ee6a3f6ab6d7a/bootstrap.sh
 
-cd $(dirname "${BASH_SOURCE}");
+cd "$(dirname "${BASH_SOURCE}")" || exit
 
 git pull origin master
 
 function spaceship() {
   # This installs the spaceship theme for zsh
   # https://github.com/denysdovhan/spaceship-prompt
-  if [ -d "$ZSH/custom/themes/spaceship-prompt" ]
-  then
+  if [ -d "$ZSH/custom/themes/spaceship-prompt" ]; then
     echo "spaceship-prompt is already installed, skipping..."
   else
     git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH/custom/themes/spaceship-prompt"
@@ -19,31 +18,34 @@ function spaceship() {
   fi
 }
 
-function italicTerm() {
-  tic -x terminfo/xterm-256color-italic.terminfo
-  tic -x terminfo/tmux-256color.terminfo
+function stowit() {
+  usr=$1
+  app=$2
+  # -v verbose
+  # -R recursive
+  # -t target
+  stow -v -R -t ${usr} ${app}
 }
 
 function doIt() {
   # install homebrew
-  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-  # install vim-plug
-  curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
-      https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
   # install oh-my-zsh
-  sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" "" --unattended
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+  git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+  git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+  ./brew.sh
 
   # link dotfiles
-  stow tmux
-  stow nvim
-  stow zsh
-  stow git
+  stowit ~ git
+  stowit ~ tmux
+  stowit ~ nvim
+  stowit ~ zsh
 
   # install nvim plugins and python addon
   pip3 install --user --upgrade neovim
-  nvim +PlugClean! +qall
-  nvim +silent +PlugInstall +qall
-  python3 ~/.config/nvim/plugged/YouCompleteMe/install.py
+  nvim --headless '+Lazy install' +qall
 
   # install tpm
   git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
@@ -52,20 +54,17 @@ function doIt() {
   spaceship
 }
 
-if [ "$1" == "--force" -o "$1" == "-f" ]; then
+if [[ "$1" == "--force" ]] || [[ "$1" == "-f" ]]; then
   doIt
-  italicTerm
   ./brew.sh
 else
-  read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
-  echo "";
+  read -p -r "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1
+  echo ""
   if [[ $REPLY =~ ^[Yy]$ ]]; then
     doIt
-    italicTerm
     ./brew.sh
-  fi;
-fi;
+  fi
+fi
 
 unset doIt
-unset italicTerm
 unset spaceship
