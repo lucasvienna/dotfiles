64f6e9fb40472f6188e80c2be48609523f1f3294
Lucas Vienna <dev@lucasvienna.dev>
2025-11-10 00:23:38 +0100

    Use Nick's script for V3

---
+++ .config/zsh/.zprofile
@@ -0,0 +1,72 @@
+# shellcheck shell=bash
+
+# This file runs once at login.
+
+# Set up a few standard directories based on:
+#   https://wiki.archlinux.org/title/XDG_Base_Directory
+export XDG_CONFIG_HOME="${HOME}/.config"
+export XDG_CACHE_HOME="${HOME}/.cache"
+export XDG_DATA_HOME="${HOME}/.local/share"
+export XDG_STATE_HOME="${HOME}/.local/state"
+
+# Explicitly set the temp directory
+export TMPDIR="/tmp"
+
+# Add all local binaries to the system path and make sure they are first.
+export PATH="$HOME/bin:$HOME/.local/bin:$HOME/.local/bin/local:$PATH"
+
+# Confiure Mise (programming language run-time manager).
+export PATH="${XDG_DATA_HOME}/mise/shims:${PATH}"
+
+# Configure GPG.
+export GNUPGHOME="${XDG_CONFIG_HOME}/gnupg"
+
+# Add colors to the less and man commands.
+export LESS=-R
+LESS_TERMCAP_ue="$(printf '%b' '[0m')"
+export LESS_TERMCAP_ue
+export LESS_TERMCAP_mb=$'\e[1;31mm'   # begin blinking
+export LESS_TERMCAP_md=$'\e[1;36m'    # begin bold
+export LESS_TERMCAP_us=$'\e[1;332m'   # begin underline
+export LESS_TERMCAP_so=$'\e[1;44;33m' # begin standout-mode - info box
+export LESS_TERMCAP_me=$'\e[0m'       # end mode
+export LESS_TERMCAP_ue=$'\e[0m'       # end underline
+export LESS_TERMCAP_se=$'\e[0m'       # end standout-mode
+
+# Configure delta (diffs) defaults.
+# https://dandavison.github.io/delta/environment-variables.html
+export DELTA_FEATURES="diff-so-fancy"
+
+# Default languages
+export LC_ALL=en_US.UTF-8
+export LANG=en_US.UTF-8
+
+# Default programs to run.
+export EDITOR="nvim"
+export DIFFPROG="nvim -d"
+
+# tell homebrew to shut up
+export HOMEBREW_NO_ENV_HINTS=1
+
+# Golang
+export PATH="/usr/local/go/bin:$PATH"
+
+# rust
+export PATH="$HOME/.cargo/bin:$PATH"
+
+# ruby gems
+export PATH="$HOME/.gem/bin:$PATH"
+
+# brew kegs that we want to replace the system tools with
+export PATH="/usr/local/opt/curl/bin:$PATH"               # curl
+export PATH="/usr/local/opt/ruby/bin:$PATH"               # ruby
+export PATH="/usr/local/opt/gnu-sed/libexec/gnubin:$PATH" # sed
+export PATH="/usr/local/opt/grep/libexec/gnubin:$PATH"    # grep, egrep, fgrep
+
+# Added by OrbStack: command-line tools and integration
+# This won't be added again if you remove it.
+source ~/.orbstack/shell/init.zsh 2>/dev/null || :
+
+# Load local settings if they exist.
+# shellcheck disable=SC1091
+if [ -f "${XDG_CONFIG_HOME}/zsh/.zprofile.local" ]; then . "${XDG_CONFIG_HOME}/zsh/.zprofile.local"; fi
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,69 +1,25 @@
 #!/usr/bin/env bash
 
-# This has been shamelessly copied from mathiasbynens/dotfiles and edited a bit
+# This has been initially copied from mathiasbynens/dotfiles and edited quite heavily
 # https://github.com/mathiasbynens/dotfiles/blob/b22c32290e1518c0f228afba254ee6a3f6ab6d7a/bootstrap.sh
 
 cd "$(dirname "${BASH_SOURCE}")" || exit
 
-git pull origin master
-
-function spaceship() {
-  # This installs the spaceship theme for zsh
-  # https://github.com/denysdovhan/spaceship-prompt
-  if [ -d "$ZSH/custom/themes/spaceship-prompt" ]; then
-    echo "spaceship-prompt is already installed, skipping..."
-  else
-    git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH/custom/themes/spaceship-prompt"
-    ln -s "$ZSH/custom/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH/custom/themes/spaceship.zsh-theme"
-  fi
-}
-
-function stowit() {
-  usr=$1
-  app=$2
-  # -v verbose
-  # -R restow (undo + redo)
-  # -t target
-  stow -verbose --restow -t "${usr}" "${app}"
-}
-
-function doIt() {
+install_brew() {
   # install homebrew
   NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-  # install oh-my-zsh
-  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
-  git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-  git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
-  ./brew.sh
-
-  # link dotfiles
-  stowit ~ git
-  stowit ~ tmux
-  stowit ~ nvim
-  stowit ~ zsh
-
-  # install nvim plugins
-  nvim --headless '+Lazy install' +qall
-
-  # install tpm
-  git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-
-  # launch the spaceship
-  spaceship
+  if [ -f "/opt/homebrew/bin/brew" ]; then
+    # Apple Silicon mac
+    eval "$(/opt/homebrew/bin/brew shellenv)" \
+      && brew install bash \
+      && bash
+  elif [ -f "/usr/local/bin/brew" ]; then
+    # Intel mac
+    eval "$(/usr/local/bin/brew shellenv)" \
+    && brew install bash \
+    && bash
+  fi
 }
 
-if [[ "$1" == "--force" ]] || [[ "$1" == "-f" ]]; then
-  doIt
-  ./brew.sh
-else
-  read -p -r "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1
-  echo ""
-  if [[ $REPLY =~ ^[Yy]$ ]]; then
-    doIt
-    ./brew.sh
-  fi
-fi
-
-unset doIt
-unset spaceship
+install_brew
