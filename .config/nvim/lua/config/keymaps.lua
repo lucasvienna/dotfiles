5289dcba837b7ce7d916e0ba4e4f8aa659682020
Lucas Vienna <dev@lucasvienna.dev>
2025-11-16 21:37:26 +0100

    Add C-d, C-u better maps

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -9,3 +9,6 @@ MAP("n", "<leader>uW", ":set list!<CR>", { desc = "Toggle WhiteSpace" })
 MAP("n", "<leader>bc", ":let @+ = expand('%:.')<CR>", { desc = "Copy Path" })
 
 MAP("n", "<leader>L", "<cmd>LazyExtras<cr>", { desc = "Lazy Extras" })
+
+MAP("n", "<C-d>", "<C-d>zz", { desc = "Half page down and center" })
+MAP("n", "<C-u>", "<C-u>zz", { desc = "Half page up and center" })
