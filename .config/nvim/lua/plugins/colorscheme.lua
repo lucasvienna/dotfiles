e64d6bc461fe455d064cf09d9d10f9c8c3dfdd9f
Lucas Vienna <dev@lucasvienna.dev>
2025-11-29 10:18:49 +0100

    Add Catppuccin Macchiato theme

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,34 +1,5 @@
 return {
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "tokyonight-moon",
-    },
-  },
-  {
-    "folke/tokyonight.nvim",
-    opts = {
-      style = "moon",
-      transparent = true,
-      styles = {
-        comments = { italic = true },
-        keywords = { italic = false },
-        sidebars = "transparent",
-        floats = "transparent",
-      },
-      on_highlights = function(hl, c)
-        hl.SnacksPickerPathHidden = { fg = c.comment }
-        hl.SnacksPickerDir = { fg = c.comment }
-        hl.SnacksPickerGitStatusIgnored = { fg = c.nontext }
-        hl.SnacksPickerGitStatusUntracked = { fg = c.nontext }
-        hl.ColorColumn = { bg = c.bg_highlight }
-        hl.SpellBad = { fg = c.red, undercurl = true }
-        hl.SpellCap = { fg = c.red, undercurl = true }
-        hl.SpellLocal = { fg = c.red, undercurl = true }
-        hl.SpellRare = { fg = c.red, undercurl = true }
-      end,
-    },
-  },
-  { "git@github.com:dracula-pro/vim.git", name = "dracula_pro" },
+  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+  { "git@github.com:dracula-pro/vim.git", name = "dracula_pro", priority = 1000 },
   { "ellisonleao/gruvbox.nvim" },
 }
