32428c5af6800c614fcbd75df11a5501dcab45eb
Lucas Vienna <dev@lucasvienna.dev>
2025-11-29 10:18:49 +0100

    Add Catppuccin Macchiato theme

--- themes/tokyonight-moon/nvim.lua
+++ themes/tokyonight-moon/nvim.lua
@@ -1,8 +1,32 @@
 return {
 	{
-		"LazyVim/LazyVim",
+		"folke/tokyonight.nvim",
 		opts = {
-			colorscheme = "tokyonight-moon",
+			style = "moon",
+			transparent = true,
+			styles = {
+				comments = { italic = true },
+				keywords = { italic = false },
+				sidebars = "transparent",
+				floats = "transparent",
+			},
+			on_highlights = function(hl, c)
+				hl.SnacksPickerPathHidden = { fg = c.comment }
+				hl.SnacksPickerDir = { fg = c.comment }
+				hl.SnacksPickerGitStatusIgnored = { fg = c.nontext }
+				hl.SnacksPickerGitStatusUntracked = { fg = c.nontext }
+				hl.ColorColumn = { bg = c.bg_highlight }
+				hl.SpellBad = { fg = c.red, undercurl = true }
+				hl.SpellCap = { fg = c.red, undercurl = true }
+				hl.SpellLocal = { fg = c.red, undercurl = true }
+				hl.SpellRare = { fg = c.red, undercurl = true }
+			end,
 		},
 	},
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "tokyonight-moon",
+    },
+  },
 }
