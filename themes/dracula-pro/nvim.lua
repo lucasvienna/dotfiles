059f67c38ed0cbeaf3dc24a27ed7af56ce51742b
Lucas Vienna <dev@lucasvienna.dev>
2025-11-30 22:55:33 +0100

    Configure Dracula Pro with all options

--- themes/dracula-pro/nvim.lua
+++ themes/dracula-pro/nvim.lua
@@ -1,5 +1,20 @@
 return {
-	{ "git@github.com:dracula-pro/vim.git", name = "dracula_pro" },
+	{
+		"git@github.com:dracula-pro/vim.git",
+		name = "dracula_pro",
+		lazy = false,
+		priority = 1000,
+		init = function()
+			-- These must be set BEFORE the colorscheme loads
+			vim.g.dracula_colorterm = 0 -- Disable background fill (enables transparency)
+			vim.g.dracula_italic = 1
+			vim.g.dracula_bold = 1
+			vim.g.dracula_underline = 1
+			vim.g.dracula_undercurl = 1
+			vim.g.dracula_strikethrough = 1
+			vim.g.dracula_full_special_attrs_support = 1
+		end,
+	},
 	{
 		"LazyVim/LazyVim",
 		opts = {
