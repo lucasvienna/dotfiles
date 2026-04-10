90e582ba2b2a91b7776ab2f8df17e0596e89ca3d
Lucas <lucas.vienna@dscsag.com>
2026-04-10 11:13:50 +0200

    Fix ghostty grammar parsing
    We were missing the tree-sitter-cli to build certain packages; also
    switched the way we import the plugin with explicit build commands
    as per the readme.

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,16 +1,16 @@
 return {
   {
+    "bezhermoso/tree-sitter-ghostty",
+    build = "make nvim_install",
+  },
+  {
     "nvim-treesitter/nvim-treesitter",
-    dependencies = {
-      "bezhermoso/tree-sitter-ghostty",
-    },
     opts_extend = { "ensure_installed" },
     opts = {
       -- Add custom treesitters not present by default in LazyVim.
       ensure_installed = {
         "csv",
         "dockerfile",
-        "ghostty",
         "go",
         "hcl",
         "helm",
