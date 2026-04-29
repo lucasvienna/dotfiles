23e3eab6ecab9de1a18cd4631967a45634a3d6e3
Lucas <lucas.vienna@dscsag.com>
2026-04-29 09:09:12 +0200

    Switch to neotest-java, add java formatter

--- .config/nvim/lua/plugins/java.lua
+++ .config/nvim/lua/plugins/java.lua
@@ -1,9 +1,35 @@
 return {
   {
+    "mason-org/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "google-java-format",
+      },
+    },
+  },
+  {
     "stevearc/conform.nvim",
     opts = {
       formatters_by_ft = {
+        -- LazyVim's Java extra ships no default formatter.
         java = { "google-java-format", stop_after_first = true },
+      },
+    },
+  },
+  {
+    "mfussenegger/nvim-jdtls",
+    opts = {
+      test = false, -- disable jdtls's test runner; we use neotest below instead.
+    },
+  },
+  {
+    "nvim-neotest/neotest",
+    dependencies = { "rcasia/neotest-java" },
+    opts = {
+      adapters = {
+        ["neotest-java"] = {
+          junit_jar = nil, -- auto-detected from Mason
+        },
       },
     },
   },
