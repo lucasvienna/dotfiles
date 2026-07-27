8151c42341b5c20fb8fe8823161ed6316dfaadac
Lucas <lucas.vienna@dscsag.com>
2026-07-27 11:35:24 +0200

    Update btop config, add shfmt for ZSH files

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -1,5 +1,13 @@
 return {
   {
+    "stevearc/conform.nvim",
+    opts = {
+      formatters_by_ft = {
+        zsh = { "shfmt" },
+      },
+    },
+  },
+  {
     "neovim/nvim-lspconfig",
     opts = {
       servers = {
